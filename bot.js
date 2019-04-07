b20a94e66130d9ec68463eaa1a7fe90950d78fed
fadedacr <47284896+fadedacr@users.noreply.github.com>
2019-04-07 11:53:40 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -23,7 +23,7 @@ client.on('message', message => {
 
     if (message.content === 'ping') {
        message.reply('pong');
-       client.user.setStatus('idle')
+       client.user.setStatus('dnd')
     }
 
 });
