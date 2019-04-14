f3924f4c70fbb82684ff2d2ad5e918ef022dfe23
fadedacr <47284896+fadedacr@users.noreply.github.com>
2019-04-13 23:36:25 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -31,6 +31,24 @@ function sendrequest(){
     });
 }
 
+client.on('message', message => {
+  // If the message is "how to embed"
+  if (message.content === 'how to embed') {
+    // We can create embeds using the MessageEmbed constructor
+    // Read more about all that you can do with the constructor
+    // over at https://discord.js.org/#/docs/main/stable/class/RichEmbed
+    const embed = new RichEmbed()
+      // Set the title of the field
+      .setTitle('A slick little embed')
+      // Set the color of the embed
+      .setColor(0xFF0000)
+      // Set the main content of the embed
+      .setDescription('Hello, this is a slick embed!');
+    // Send the embed to the same channel as the message
+    message.channel.send(embed);
+  }
+});
+
 function updatename(){
   client.user.setActivity('Re-Imagined Networks', { type: 'STREAMING', url: 'https://www.twitch.tv/urmom'})
   .then(presence => console.log(`Activity set to ${presence.game ? presence.game.name : 'none'}`))
